30093fe626e7264ee36466f881fabf2a6e78a44e
Taejun Kim <129062832+taejun20@users.noreply.github.com>
2025-10-17 14:33:03 +0900

    u

---
+++ img/main.py
@@ -0,0 +1,21 @@
+from PIL import Image
+import os
+
+# Directory containing PNG images
+input_dir = "."
+output_dir = "."
+
+# Make sure the output directory exists
+os.makedirs(output_dir, exist_ok=True)
+
+filename = "profileImage.png"
+
+# Open the image
+img_path = os.path.join(input_dir, filename)
+img = Image.open(img_path).convert("RGB")  # convert to RGB to save as JPEG
+
+jpg_path = os.path.join(output_dir, f"profileImage2.jpg")
+# Save as JPEG
+img.save(jpg_path, "JPEG", quality=5)
+
+print("✅ All PNGs converted to JPGs.")
